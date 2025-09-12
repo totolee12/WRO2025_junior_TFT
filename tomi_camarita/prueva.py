8043bcad1552e67cf36975e40f492768b6cc739a
johanavaldiviezo <johanavaldi@gmail.com>
2025-09-12 12:26:28 -0300

    add camera

---
+++ tomi_camarita/prueva.py
@@ -0,0 +1,16 @@
+lista = []
+print (lista)
+
+lista.append(5)
+
+print(lista)
+
+lista.append("hola")
+print(lista)
+
+lista.append("hola")
+print(lista[0])
+print(lista)
+
+lista[1] = True
+print(lista)
