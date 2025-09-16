b98d894d57a8315817022440dfea7f80214fe952
tomiargacaba <tomiargacaba@gmail.com>
2025-09-16 19:55:15 -0300

    Add files via upload
    main que arreglaron cosas

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     Funciones.bajar_pala(260)                       #baja la pala
     Funciones.subir_brazo(85)                       #sube el brazo
     Funciones.mover_con_pid_sin_reiniciar(200,-180) #avanza a juntar la primera pelota
-    Funciones.mover_con_pid_sin_reiniciar(-200,-180)#retrocede para acomodar
-    Funciones.subir_pala(100)                       #sube pala y acomoda la pelota
-    Funciones.bajar_pala(100)                       #vuelve a bajar pala
-    Funciones.mover_con_pid_sin_reiniciar(200,-180) #va a juntar la otra pelota
+    Funciones.mover_con_pid_sin_reiniciar(-100,-180)#retrocede para acomodar
+    Funciones.subir_pala(80)                        #sube pala y acomoda la pelota
+    Funciones.bajar_pala(80)                        #vuelve a bajar pala
+    Funciones.mover_con_pid_sin_reiniciar(100,-180) #va a juntar la otra pelota
     Funciones.mover_con_pid_sin_reiniciar(-150,-180)#retrocede para irse a dejarlas
     Funciones.giro_der(-90)                         #gira 90 a la derecha
     Funciones.mover_con_pid_sin_reiniciar(400,-90)  #avanza a dejar las pelotas
@@ -43,7 +43,27 @@ if __name__ == "__main__":
     Funciones.giro_der(-180)                        #gira para ir hasta el dron
     Funciones.mover_con_pid_sin_reiniciar(850,-180) #abanza para dejar el dron
     Funciones.mover_con_pid_sin_reiniciar(-850,-180)#retrocede un poco para acomodarse
-    Funciones.giro_der(-90)                         #
-    Funciones.mover_con_pid_sin_reiniciar(-100,-90) #
-    Funciones.mover_con_pid_sin_reiniciar(30,-90)   #
-    Funciones.bajar_pala(75)                        #
+    Funciones.giro_der(-90)                         #gira para acomodarse contra la pared
+    Funciones.mover_con_pid_sin_reiniciar(-100,-90) #retrocede y se acomoda
+    Funciones.mover_con_pid_sin_reiniciar(30,-90)   #se mueve adelante para buscar bloques
+    Funciones.bajar_pala(75)                        #baja la pala
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
